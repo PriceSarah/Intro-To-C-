e670821c5a56a9ad0f6706fee4e774986934ac96
Sarah Price <s198041@students.aie.edu>
2019-08-19 15:00:10 -0500

    Add project files.

---
+++ Learning Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Variables
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, What is your name?");
+            //initializes variables. Can be left empty or blank
+            string playerName = "";
+            int playerScore = 0;
+
+            playerName = Console.ReadLine();
+
+            int add = 0;
+
+            while (playerScore < 10) {
+                Console.Write("Increment Score: ");
+                add = Convert.ToInt32(Console.ReadLine());
+            playerScore = playerScore + add;
+            Console.Write("Current Score = ");
+            Console.WriteLine(playerScore);
+        }
+           
+            //Writes the lines and adds variables to code
+            Console.WriteLine(playerName + " Wins!");
+            
+            //outputs the code
+            Console.ReadLine();
+        }
+    }
+}
